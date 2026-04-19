d07bf0940721e87d2efbaa94577a623c56e0d995
riccardo-talarico <riccardotalarico363@gmail.com>
2026-04-19 15:23:26 +0200

    Add Node and basic nodes

---
+++ autodiff/nodes/node.py
@@ -0,0 +1,93 @@
+from value import Value
+import numpy as np
+
+class Node(object):
+    def __init__(self, requires_grad: bool):
+        self.parents = []
+        self.children = []
+
+        self.requires_grad = requires_grad
+        self.value: Value | None = None
+        self.inputs = None
+
+        self.local_grad : list[Value] = []
+        self.upstream_grad : Value = Value(0)
+        self.inputs = None
+
+    def get_inputs(self) -> list[Value] | None:
+        """
+        Gets the inputs from the parent nodes
+        """
+        if len(self.parents)==0:
+            return self.inputs
+        self.inputs = []
+        for parent in self.parents:
+            self.inputs.append(parent.value)
+
+        return self.inputs
+    
+    def backward(self):
+        """
+        Updates the upstream gradient of the parents with the corresponding local gradient.
+        """
+        for i,parent in enumerate(self.parents):
+            parent.upstream_grad += self.local_grad[i] 
+
+
+class AddNode(object):
+    def forward(self):
+
+        x,y = self.get_inputs()
+        self.x = x
+        self.y = y
+
+        self.value = x+y
+        return self.value
+    
+    def backward(self):
+        # upstream_grad = dL/dz
+        dx = self.upstream_grad
+        dy = self.upstream_grad
+        if self.requires_grad:
+            self.local_grad = [dx, dy] 
+        # updates the parents grad
+        super().backward()
+        return self.local_grad
+    
+class SubNode(Node):
+    def forward(self):
+        x,y = self.get_inputs()
+        self.x = x
+        self.y = y
+
+        self.value = x-y
+        return self.value
+    
+    def backward(self):
+        # upstream_grad = dL/dz
+        dx = self.upstream_grad
+        dy = Value((-1)*self.upstream_grad.value)
+        if self.requires_grad:
+            self.local_grad = [dx, dy] 
+        # updates the parents grad
+        super().backward()
+        return self.local_grad
+
+class MatrixMultNode(Node):
+    def forward(self):
+        A,B = self.get_inputs()
+        self.A = A
+        self.B = B
+        self.value = A @ B
+        return self.value
+    
+    def backward(self):
+        # upstream_grad = dL/dz
+        dA = self.upstream_grad @ self.B.T # [dz/dA * dL/dz] 
+        dB = self.A.T @ self.upstream_grad # [dz/dB * dL/dz]
+        
+        if self.requires_grad:
+            self.local_grad = [dA, dB]
+        super().backward()
+
+        return self.local_grad
